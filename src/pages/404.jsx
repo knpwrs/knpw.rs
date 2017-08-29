21e48bb300c82cc7a571c286f8ef133f4bae42ae
Kenneth Powers <ken@kenpowers.net>
2017-08-28 22:27:35 -0400

    Rewrite 404 page.

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import CenterWrap from '../components/center-wrap';
 
 export default () => (
-  <div>
-    <Helmet>
-      <title>404 &middot; Ken Powers</title>
-    </Helmet>
-    <section>
-      <div>
-        <header>
-          <div>
-            <h1>AW NO! The page you are trying to access is not here.</h1>
-          </div>
-        </header>
-      </div>
-    </section>
-  </div>
+<CenterWrap>
+  <Helmet>
+    <title>404 &middot; Ken Powers</title>
+  </Helmet>
+  <h2>404</h2>
+  <p>Whoops! I can't find what you're looking for.</p>
+</CenterWrap>
 );
