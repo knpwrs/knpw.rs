82db5040736170cc5fee3f753d915734e6f4ca0b
Ken Powers <ken@kenpowers.net>
2025-04-17 09:18:03 -0400

    Add giscus back

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import solidJs from '@astrojs/solid-js';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://knpw.rs',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), solidJs()],
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
