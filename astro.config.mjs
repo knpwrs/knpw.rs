9bee74a8adf0fad9e42435f27424fc70f675797e
Ken Powers <ken@kenpowers.net>
2026-03-10 16:00:18 -0400

    Upgrade to Astro 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import solidJs from '@astrojs/solid-js';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://knpw.rs',
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Comic Mono',
+      cssVariable: '--font-comic-mono',
+    },
+  ],
   integrations: [mdx(), sitemap(), solidJs()],
   redirects: {
     '/blog/[...slug]': '/blg/[...slug]',
